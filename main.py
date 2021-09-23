ed7d6aeebcbcd03f7530992f1cf22c467ac8aa6d
JensLidman <64350232+JensLidman@users.noreply.github.com>
2021-09-23 09:24:53 +0200

    added data and most of the code

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import numpy as np
+
+
+def getDataFromFileList():
+    data = {}
+    fileList = os.listdir()
+    for i in range(len(fileList)):
+        
+        
+    
+def main():
+    
+    getDataFromFileList()
+	
+
+if __name__ == "__main__":
+	main()
