e7ee3ad847ad8c40d7b931b0f9f83eca60cf61c4
JensLidman <64350232+JensLidman@users.noreply.github.com>
2021-11-01 10:42:29 +0100

    Fixed bugs and added new info to README

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ def getFileInfo(fileName):
 	- type of file is essential for reading mode
 	"""
 	info = {}
-	arr = re.match(r'cpmg_ch3_mq_(\d+)ms_(\d+)mhz_(\d+)c.(\w+)',fileName)
-	info["delay"]=arr.group(1)
-	info["magnet"]=arr.group(2)
-	info["temp"]=arr.group(3)
-	info["type"]=arr.group(4)
+	arr = re.match(r'.+cpmg_ch3_mq_(\d+)ms_(\d+)mhz_(\d+)c.(\w+)',fileName)
+	if(arr):
+		info["delay"]=arr.group(1)
+		info["magnet"]=arr.group(2)
+		info["temp"]=arr.group(3)
+		info["type"]=arr.group(4)
 	return info
 
 
@@ -38,7 +39,7 @@ def readExpFile(fileName,type):
 	file = open(fileName,'r', encoding='utf8')
 	
 	allDataFromFile = file.read()
-	allRes = allDataFromFile.split("\n\n")
+	allRes = allDataFromFile.split("\n\n") # Each dataset is seperated by two linebreaks
 	file.close()
 	
 	for res in allRes:
@@ -54,6 +55,7 @@ def readExpFile(fileName,type):
 			elif(str.startswith("#")):
 				continue
 			elif type == "fit":
+				dataset["dataexist"]=True
 				strList = str.split()
 				if len(strList)==3:
 					values.append([strList[0],strList[2]])
@@ -76,7 +78,8 @@ def getDataFromFileList(filedir):
 	[[info],[[residue],[data]]]
 	"""
 	data = []
-	filelist = os.listdir(filedir)
+	filelist =os.listdir(filedir)
+	
 	print("Loading data from data dir\n")
 	if len(filelist)>0:
 		print("DataFiles included:\n ----------------------------------")
@@ -88,24 +91,33 @@ def getDataFromFileList(filedir):
 		if "cpmg" not in f :
 			continue
 		d1 ={}
-		d1["info"]= getFileInfo(f)
+		d1["info"]= getFileInfo(filedir+f)
+		if not d1["info"]:
+			return data
 		
 		if not d1["info"]["type"]  =="fit" and not d1["info"]["type"] == "exp": #This is to avoid additional files that have the same name
 			continue
 		else:
 			print("|  "+f+" |")
-		d1["resdata"]=readExpFile(f,d1["info"]["type"])
+		d1["resdata"]=readExpFile(filedir+f,d1["info"]["type"])
 		
 		
 		data.append(d1)
 	print(" ----------------------------------\n")
 
 	return data
-def getExpDataMean(data,magnet,index):
+def getExpDataMean(data,magnet,resName):
 	for expData in data:
 		if  expData["info"]["type"] == "exp" and not expData["info"]["magnet"] == magnet:
 			totErrUp = []
 			totErrDown = []
+			index = -1
+			for i in range(len(expData["resdata"])):
+				if expData["resdata"][i]["name"]==resName:
+					index = i
+					break
+			if index ==-1:
+				return 0.0,0.0
 			for value in expData["resdata"][index]["values"]:
 				totErrUp.append(float(value[2]))
 				totErrDown.append(float(value[3]))
@@ -123,7 +135,7 @@ def renameRes(res):
 	elif "CB" in res: return res.replace("CB","\\xb")
 	else: print(res+ " could not be converted")
 	
-def writeToR2GraceFile(magnet,R2,R2_name):
+def writeToR2GraceFile(magnet,R2,R2_name,missingRes,width):
 	"""
 	Writes the created R2 values to a existing tempalate file (This file have all the information of how the plot is supposed to look)
 	The template have the syntac INSERT_AXIS for the names, and INSERT_DATA for the R2 values
@@ -132,7 +144,9 @@ def writeToR2GraceFile(magnet,R2,R2_name):
 	template_R2 = open(TEMPLATE_GRACE_R2,'r')
 	template = template_R2.read()
 	template = template.replace("INSERT_AXIS",R2_name)
-	template = template.replace("INSERT_DATA",R2)
+	template = template.replace("INSERT_DATA_R2",R2)
+	template = template.replace("INSERT_MISSING",missingRes)
+	template = template.replace("AXIS_WIDTH",width)
 	file_R2.write(template)
 	file_R2.close()
 	template_R2.close()
@@ -145,12 +159,34 @@ def writeToR2pymolFile(magnet,R2):
 	for i in R2:
 		file_R2.write(i[0]+"\t"+i[1]+"\n")
 	file_R2.close()
+	
 def arrangepeaklist(plist):
-	list = sorted(plist,key=lambda x: x[1:])
+	"""
+	Arranges the peaklist (if it is imported) with a sorting function and replacing certain characters to match
+	the rest of the data.
+	"""
+	pFile = open(plist,'r')
 	peaklist = []
-	for peak in plist:
-		l = peak.strip().split(" ")[0]
-		print(l)
+	for peak in pFile:
+		if "#" in peak:
+			continue
+		l = peak.strip().split()[0]
+		if "-" in l:
+			l = l.replace("-","C")
+		peaklist.append(l)
+	return sorted(peaklist,key=lambda x: x[1:])
+	
+def addMissingRes(data,plist):
+	"""
+	This will simply add a empty peak, if it is missing. With a key "dataexist" set as False
+	"""
+	for i in range(len(plist)):
+		if not data[i]["name"]==plist[i]:
+			dataset ={}
+			dataset["name"]=plist[i]
+			dataset["dataexist"]=False
+			data.insert(i,dataset)
+	return data
 		
 def calcR2(data,plist):
 	"""
@@ -158,28 +194,43 @@ def calcR2(data,plist):
 	There is no error in the fitted data, to get som errors they are taken from the experimental data.
 	First the the correct detasets are extracted (info then type from directory) to be fit.
 	"""
-	peaklist = arrangepeaklist(plist)
+	peaklist = []
+	if len(plist)>0:
+		peaklist = arrangepeaklist(plist)
 
 	fitDataList = []
 	for d in data:
 		if(d["info"]["type"]=="fit"):
 			fitDataList.append(d)
-	for fitData in fitDataList:
-		
+	for d in fitDataList:
+		fitData = addMissingRes(d["resdata"],peaklist)
 		R2 = ""
-		R2_name= "@    xaxis  tick spec "+str(len(fitData["resdata"]))+"\n"
+		R2_axis= "@    xaxis  tick spec "+str(len(fitData))+"\n"
+		R2_agr_axis = ""
 		R2_pymol = []
-		for i in range(len(fitData["resdata"])):
+		missingValues = []
+		
+		for i in range(len(fitData)):
 				#import pdb; pdb.set_trace()
-				
-			diff = float(fitData["resdata"][i]["values"][0][1])-float(fitData["resdata"][i]["values"][-1][1])
-			errUp,errDown = getExpDataMean(data,fitData["info"]["magnet"],i)
-			R2_name+= '@    xaxis  ticklabel '+str(i)+', "'+renameRes(fitData["resdata"][i]["name"])+'"\n@    xaxis  tick major '+str(i)+', '+str(i)+'\n'
+			diff = 0
+			if  fitData[i]["dataexist"]:
+				diff = float(fitData[i]["values"][0][1])-float(fitData[i]["values"][-1][1])
+			else:
+						missingValues.append([i,1])
+			errUp,errDown = getExpDataMean(data,d["info"]["magnet"],fitData[i]["name"])
+			
+			R2_axis+= '@    xaxis  ticklabel '+str(i)+', "'+renameRes(fitData[i]["name"])+'"\n@    xaxis  tick major '+str(i)+', '+str(i)+'\n'
 			R2 += str(i)+"\t"+str(diff)+"\t"+str(errUp)+"\t"+str(errDown)+"\n"
-			R2_pymol.append([fitData["resdata"][i]["name"],str(diff)])
-		R2 += "&"
-		writeToR2GraceFile(fitData["info"]["magnet"],R2,R2_name)
-		writeToR2pymolFile(fitData["info"]["magnet"],R2_pymol)
+			if fitData[i]["dataexist"]:
+				R2_pymol.append([fitData[i]["name"],str(diff)])
+			else:
+				R2_pymol.append([fitData[i]["name"],"NULL"])
+		missingRes = ""
+		for m in missingValues:
+			missingRes += str(m[0])+"\t"+str(m[1])+"\n"
+		width = str(float(len(fitData))-0.5)
+		writeToR2GraceFile(d["info"]["magnet"],R2,R2_axis,missingRes,width)
+		writeToR2pymolFile(d["info"]["magnet"],R2_pymol)
 
 def writeResToGrace(dataTot):
 	"""
@@ -246,18 +297,19 @@ def createResultFolder():
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-n", "--name", action="store", nargs="?", help="Name of datafolder")
-		parser.add_argument("-p", "--peaklist", action="store", nargs="?", help="Reference peaklist")
+	parser.add_argument("-p", "--peaklist", action="store", nargs="?", help="Reference peaklist")
 	args = parser.parse_args()
     
-
+	print(args.name)
 
 	data = getDataFromFileList(args.name)
     
 	if len(data)>0:
 		if checkData(data):
 			createResultFolder()
-			calcR2(data)
 			writeResToGrace(data)
+
+			calcR2(data,args.peaklist)
 	else:
 		print("No data found")
 
